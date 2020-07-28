1a47edf0365c0257803eef8e74c3fa9cc7b3680c
Anton Vaitsehovskiy <a.vasi1yev@yandex.ru>
2020-07-28 20:21:32 +0300

    cors

--- app.js
+++ app.js
@@ -38,32 +38,29 @@ const limiter = rateLimit({
   max: 100,
 });
 
-const whitelist = [
-  'http://localhost:8080',
-  '84.201.169.56',
-  'http://myownnews.ru.com/',
-  'https://myownnews.ru.com/',
-  'http://myownnews.ru.com/api',
-  'https://myownnews.ru.com/api',
-  'http://www.myownnews.ru.com/',
-  'https://www.myownnews.ru.com/',
-  'http://www.myownnews.ru.com/api',
-  'https://www.myownnews.ru.com/api',
-  'https://Vaitsehovskiy-Tony.github.io',
-];
-
 const corsOptions = {
-  origin(origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true, // для передачи заголовка Access-Control-Allow-credentials
+  origin: [
+    'http://localhost:8080',
+    '84.201.169.56',
+    'http://myownnews.ru.com/',
+    'https://myownnews.ru.com/',
+    'http://myownnews.ru.com/api',
+    'https://myownnews.ru.com/api',
+    'http://www.myownnews.ru.com/',
+    'https://www.myownnews.ru.com/',
+    'http://www.myownnews.ru.com/api',
+    'https://www.myownnews.ru.com/api',
+    'https://Vaitsehovskiy-Tony.github.io',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowerHeaders: ['Content-Type'],
+  credentials: true,
 };
 
-app.use(cors(corsOptions));
+app.use('*', cors(corsOptions));
+
 app.use(limiter);
 app.use(helmet());
 app.use(cookieParser());
